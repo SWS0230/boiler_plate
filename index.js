a852956d62dcea94031ada6a4ab181d12b54956e
SWS0230 <43507859+SWS0230@users.noreply.github.com>
2024-09-01 07:25:58 +0000

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 const port = 5000;
+const host = "localhost";
+const bodyParser = require("body-parser");
+const config = require('./boiler-plate/config/key')
+const { User } = require("./boiler-plate/models/User");
 
-const mongoose = require("mongoose")
-mongoose.connect('mongodb+srv://sunwoosong33:Dumbdudumb5868*@boilerplate.fipoa.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate', {
-  userNewUrlParser: true, useUnifiendTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("Hello World!, 안녕하세요"));
+//application/json
+app.use(bodyParser.json());
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const mongoose = require("mongoose");
+const { error } = require("console");
+mongoose
+  .connect(
+    "mongodb+srv://sunwoosong33:Dumbdudumb5868*@boilerplate.fipoa.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate",
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  //회원가입할 때 필요한 정보들을 client에서 가져오면 그것들을 db에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+app.listen(port, host, () => console.log(`Example app listening on port ${port}!`));
